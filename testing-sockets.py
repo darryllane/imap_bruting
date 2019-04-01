9e90c80fb668465db5c8b37d6a7eba3411e61ffd
Darryl Lane <darryllane101@gmail.com>
2019-04-02 00:29:28 +0100

    0.0.1a

--- testing-sockets.py
+++ testing-sockets.py
@@ -1,31 +1,99 @@
 import imaplib
 import pprint
 import argparse
+import socket
+import ssl
+import re
+import os
+import sys
 
-imap_host = 'imap.gmail.com'
-imap_user = 'darryllane101@gmail.com'
-imap_pass = ''
 
-
-# Create parser and suppress help to utilise our own
+# Create parser
 parser = argparse.ArgumentParser(prog='imap_brute', description='IMAP bruting', add_help=False)
 
-parser.add_argument('-h', help='host', required=False)
-parser.add_argument('-H', help='file with hosts', required=False)
+host_group = parser.add_mutually_exclusive_group(required=True)
+host_group.add_argument('-h', help='host')
+host_group.add_argument('-H', help='file with hosts')
+
+
+user_group = parser.add_mutually_exclusive_group(required=True)
+user_group.add_argument('-user', help='username')
+user_group.add_argument('-U', help='file with usernames')
+
+password_group = parser.add_mutually_exclusive_group(required=True)
+password_group.add_argument('-pass', help='password')
+password_group.add_argument('-P', help='file with passwords')
+
+parser.add_argument('-tls', help='port', required=False, action='store_true')
 parser.add_argument('-p', help='port', required=False)
-
-parser.add_argument('-user', help='username', required=False)
-parser.add_argument('-U', help='file with usernames', required=False)
-parser.add_argument('-pass', help='password', required=False)
-parser.add_argument('-P', help='file with passwords', required=False)
-
 parser.add_argument('--help', '-help', help='help menu', required=False, action='store_true')
 parser.add_argument('-v', help='version', required=False, action='store_true')
 parser.add_argument('-V', help='verbose', required=False, action='store_true')
 
 args = vars(parser.parse_args())
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ssl_sock = ssl.wrap_socket(sock=sock, ssl_version=ssl.PROTOCOL_TLSv1)
-ssl_sock.connect((args['h'], 993))
-print()
+def get_file(args):
+	filename = args['P']
+	if not os.path.exists(filename):
+		print('The file does not exist: {}'.format(filename))
+		
+		sys.exit()
+	else:
+		data = [line.rstrip('\n') for line in open(filename)]
+		return data
+		
+
+def host_connect(args):
+	if args['tls']:
+		if args['p']:
+			imaplib.IMAP4_PORT = args['p']
+			
+		imapObj = imaplib.IMAP4_SSL(args['h'])
+	else:
+		if args['p']:
+			imaplib.IMAP4_PORT = args['p']
+			
+		imapObj = imaplib.IMAP4(args['h'])
+	
+	return imapObj
+		
+
+def list_boxes(conn):
+	status, labels = conn.list()
+	if status == 'OK':
+		respone = []
+		for label in labels:
+			#print(label.decode())
+			children, folder = label.decode().replace('"', '').replace('\\', '').replace(' Junk', '').replace(' Trash', '').split(' / ')
+			has_match = re.match('\(HasNoChildren.*|\(Marked HasNoChildren.*', children)
+			has_child = re.match('\(HasChildren.*', children)
+			if has_match:
+				children = ''
+			if has_child:
+				children = 'Has Children'
+			respone.append((folder, children))		
+	return respone
+
+if __name__ == '__main__':
+	if args['H']:
+		host_list = get_file(args)
+	
+	if args['P']:
+		pass_list = get_file(args)
+	
+	if args['U']:
+		user_list = get_file(args)
+	
+	if args['h']:
+		conn = host_connect(args)
+		
+		conn.login(args['user'], args['pass'])
+		
+		response = list_boxes(conn)
+		print('Connected....listing folders\n')
+		for folder, children in response:
+			print(folder, children)
+	
+	
+	
+
